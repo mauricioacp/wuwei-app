3fb9ce498a6b16195586d39b6e840d569d00f001
Mauricio Cavallini <mauricio1acp@hotmail.com>
2023-02-11 15:31:23 +0100

    initial

---
+++ apps/wuwei/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'wuwei-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+    title = 'wuwei';
+}
